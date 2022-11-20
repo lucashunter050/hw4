f9cb0e1a0978e4985cc067a6c1bf4e1cb4e10134
Lucas Hunter <lphunter@usc.edu>
2022-11-19 22:10:50 -0800

    gitignore and q1 updated

--- equal-paths-test.cpp
+++ equal-paths-test.cpp
@@ -11,6 +11,13 @@ Node* d;
 Node* e;
 Node* f;
 
+Node* t;
+Node* v;
+Node* w;
+Node* x;
+Node* y;
+Node* z;
+
 void setNode(Node* n, int key, Node* left=NULL, Node* right=NULL)
 {
   n->key = key;
@@ -55,6 +62,22 @@ void test5(const char* msg)
   cout << msg << ": " <<   equalPaths(a) << endl;
 }
 
+void test6(const char* msg)
+{
+  setNode(a,1,b,c);
+  setNode(b,2,d,t);
+  setNode(c,3,v,w);
+  setNode(d,4,x,NULL);
+  setNode(t,5,NULL,NULL);
+  setNode(v,6,NULL,NULL);
+  setNode(w,7,NULL,NULL);
+  setNode(x,8,NULL,NULL);
+  //setNode(y,9,b,c);
+  //setNode(z,10,NULL,d);
+
+  cout << msg << ": " << static_cast<bool>(equalPaths(a)) << endl;
+}
+
 int main()
 {
   a = new Node(1);
@@ -62,11 +85,20 @@ int main()
   c = new Node(3);
   d = new Node(4);
 
+  t = new Node(5);
+  v = new Node(6);
+  w = new Node(7);
+  x = new Node(8);
+  y = new Node(9);
+  z = new Node(10);
+
   test1("Test1");
   test2("Test2");
   test3("Test3");
   test4("Test4");
   test5("Test5");
+
+  test6("Test6");
  
   delete a;
   delete b;
